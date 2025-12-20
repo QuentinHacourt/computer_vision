4b0f7bd8433dca601d4555bd2aaa2ade2a726343
quentinhacourt <quentin.hacourt@gmail.com>
2025-12-20 20:55:54 +0100

    run script

--- code/run.sh
+++ code/run.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env sh
 
-python -m venv venv
-source venv/bin/activate
-pip install -r requirements.txt
+# if -i || venv doesn't exist
+DIR="./venv/"
+
+if [ ! -d "$DIR" ] || [ "$1" = "-i" ]; then
+    echo "Setting up virtual environment :)"
+    python -m venv venv
+    source venv/bin/activate
+    pip install -r requirements.txt
+fi
+
+venv/bin/python code/main.py
